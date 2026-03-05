3b71b8edabf9462f5118f765a1d73151deb9560e
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:53:20 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:53:07.551Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T13:53:21.287Z
+Commit: Fix broken links in documentation
