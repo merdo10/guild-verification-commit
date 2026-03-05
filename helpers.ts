539aa7eda28d5d03767de7aad18a1369e37d34d0
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:55:15 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:55:03.160Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T13:55:16.856Z
+Commit: Add dark mode support
