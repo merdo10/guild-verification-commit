468886e87f297d5a337df9fe513a716a895b69f1
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:52:33 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:52:31.822Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T13:52:34.568Z
+Commit: Resolve timezone issues in date handling
