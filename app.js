5239758cd8d32e0dcd4007fa4679c898d3b3ae21
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:56:49 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:56:45.189Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T13:56:51.157Z
+Commit: Fix CSS styling bug on mobile devices
