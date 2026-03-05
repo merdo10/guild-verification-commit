6e53c0c573eb58df578710beef0f14511e255f34
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:57:42 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:57:28.905Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T13:57:43.703Z
+Commit: Resolve timezone issues in date handling
