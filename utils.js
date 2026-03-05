05fb5f329ac81287952f5422d2277dfc87c52a27
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:55:44 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:55:42.383Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T13:55:45.399Z
+Commit: Fix memory leak in event handlers
