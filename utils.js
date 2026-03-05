f184925f148961fe31e46c073b577454e25582dc
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:55:31 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:55:02.133Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T13:55:33.148Z
+Commit: Fix race condition in async operations
