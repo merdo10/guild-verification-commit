eae52add5f6d6e6adadb47147009d3935ef91f15
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:58:13 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:57:30.036Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T13:58:14.338Z
+Commit: Resolve null pointer exception in user service
