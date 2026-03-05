1e3e1fe5b90a5c7ebae2f8645695b11bf5914d76
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:57:15 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:57:10.866Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T13:57:16.994Z
+Commit: Implement user profile customization
