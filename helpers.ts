695546a9fa0b1b6eb1a6e4a6587127fef6719d82
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:57:20 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:57:16.994Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T13:57:21.996Z
+Commit: Fix memory leak in event handlers
