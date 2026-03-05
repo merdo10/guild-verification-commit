6f0473db2c04734ebc34fbfbb00bca086c112f5f
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:54:39 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:53:40.818Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T13:54:40.612Z
+Commit: Fix database connection timeout
