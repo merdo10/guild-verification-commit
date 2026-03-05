27478926af55d28e7a486b151af91abe60381cda
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:54:53 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:54:52.763Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T13:54:54.811Z
+Commit: Fix memory leak in event handlers
