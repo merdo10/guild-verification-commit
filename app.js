ed4880be0d230b262efec927d7ac1d323e080a88
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:52:27 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:52:15.346Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T13:52:29.077Z
+Commit: Fix broken links in documentation
