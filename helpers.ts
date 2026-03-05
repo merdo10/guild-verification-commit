66aecf041038d60df138747415aa2588d1a5726e
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:57:27 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:57:21.996Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T13:57:28.905Z
+Commit: Patch API endpoint error handling
