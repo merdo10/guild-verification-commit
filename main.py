1640e854a8fe6469d78dd5151c4c495f10763855
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:57:05 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:57:04.195Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T13:57:07.244Z
+Commit: Resolve null pointer exception in user service
