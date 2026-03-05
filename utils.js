8556360e024f514fffc94c713f423a2a03562535
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:57:28 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:57:27.932Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T13:57:30.036Z
+Commit: Resolve timezone issues in date handling
