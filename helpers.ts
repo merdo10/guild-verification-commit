8f87004b83e324680d611a813d658bb974f842d5
merdo10 <mertkanat777@gmail.com>
2026-03-05 16:58:09 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:58:04.856Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T13:58:10.302Z
+Commit: Fix broken links in documentation
